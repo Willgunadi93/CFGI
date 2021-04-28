5016b888183acda7267e15f3c91f0ed2cc02a194
Victoria Rong <zhiqir@uci.edu>
2021-04-28 04:24:05 -0700

    “description”

--- app/screens/screens.js
+++ app/screens/screens.js
@@ -6,6 +6,11 @@ import { AuthContext } from '../screens/context';
 //import { TextInput } from 'react-native-gesture-handler';
 import { ScreenContainer } from 'react-native-screens';
 
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+ } from 'react-native-responsive-screen';
+
 export const HomeScreen = () => {   //home page component
 
     const { signOut } = React.useContext(AuthContext);
@@ -17,74 +22,62 @@ export const HomeScreen = () => {   //home page component
     );
   }
   
-export const JobScreen = () => {   //Jobs page component
+  export const JobScreen = () => {   //Jobs page component
     return (
-      <ScrollView style={styles.scrollView}>
-      <ScreenContainer style={StyleSheet.container}>
-      <View style={{paddingVertical: 30, alignItems: 'center'}}>
-        <Image style={{marginTop: 10}} source={require('../assets/img/logo-full.png')} />
-      </View>
-
-      <View style={{width: 104, height: 29, left: 34, top: 5}}> 
-        <Text style={{fontSize: 24, color: "#FF6E00", fontWeight: 'bold', lineHeight: 29}}>CAREER</Text>
-      </View>
-
-      <View style={{width: 309, height: 60, left: 36, top: 10}}> 
-        <Text style={{fontSize: 24, color: "#3F3356", fontWeight: 'bold', lineHeight: 30}}>Find A Job From Trusted Partners</Text>
-      </View>
-
-      <View style={{width: 308, height: 60, left: 37, top: 15}}> 
-        <Text style={{fontSize: 13, color: "#000000", lineHeight: 16}}>
+      <ScrollView>
+      <View style={styles.jobContainer}>
+      <View style={{padding: hp('5%'), flex: 1}}>
+        <Image style={{height:'30%', resizeMode:"contain", alignSelf:'center'}} source={require('../assets/img/logo-full.png')} />
+        <Text style={styles.jobTitle}>CAREER</Text>
+        <Text style={styles.jobSubtitle}>Find A Job From Trusted Partners</Text>
+        <Text style={{fontSize: 13, color: "#000000", lineHeight: 16, paddingBottom: hp('2%')}}>
           These companies took the CFGI diversity pledge{'\n'}
           and are open to considering sponsorship for the right candidates.
         </Text>
       </View>
 
       <View style={styles.inputContainer}>
-          <View style={{paddingVertical: 5}}>
-            <TextInput
-                style={styles.input}
-                placeholder="          job title, keywords, or company"/>
+          <View style={{paddingVertical: hp('1%')}}>
+            <TextInput style={styles.input} placeholder="          job title, keywords, or company"/>
           </View>
-          <View style={{paddingVertical: 5}}>
-            <TextInput
-                style={styles.input}
-                placeholder="          zipcode or city"/>
+          <View style={{paddingVertical: hp('1.2%')}}>
+            <TextInput style={styles.input} placeholder="          zipcode or city"/>
           </View>
       </View>
 
-      <View style={{width: 153, height: 35, left: 2, top: 30}}>
-        <Pressable style={styles.button}>
+      <View style={{height: hp('5%'), right: hp('12.5%'), top: hp('4.5%')}}>
+        <Pressable style={styles.jobButton}>
             <Text style={styles.textStyle}>SEARCH</Text>
         </Pressable>
       </View>
 
-      <View style={{width: 98, height: 42, left: 36, top: 170}}> 
+      <View style={{width: hp('12%'), height: hp('5%'), right: hp('12.5%'), top: hp('21.5%')}}> 
         <Text style={{fontSize: 16, color: "#000000", fontWeight: 'bold'}}>105 Results</Text>
       </View>
 
       <View style={{top: 150, paddingStart: 22, paddingEnd: 16}}> 
-        <Card style={styles.cardLayout} name="card"  pointerEvents="none">
-          <Text style={{fontSize: 13, paddingLeft: 193, color: '#3C65CC',  marginBottom: 7, marginTop: -7}} >UI/UX Design </Text>
-          <Text style={{fontSize: 16, left: 45, top: -5, fontWeight: 'bold'}} >SaaS Homepage redesign and onboarding updates </Text>
-          <Text style={{fontSize: 13, left: 45, color: '#50555C', marginTop: 2}} >Albany, NY </Text>
-          <Text style={{fontSize: 11, paddingLeft: 149, color: '#929599', marginBottom: -8, marginTop: 13}} >Posted 51 minnutes ago </Text>
+        <Card containerStyle={styles.cardLayout} name="card"  pointerEvents="none">
+          <Text style={styles.positionCategory} >UI/UX Design </Text>
+          <Text style={styles.positionTitle} >SaaS Homepage redesign and onboarding updates </Text>
+          <Text style={styles.positionLocation} >Albany, NY </Text>
+          <Text style={styles.postTime} >Posted 51 minnutes ago </Text>
         </Card>
-        <Card style={styles.cardLayout} name="card"  pointerEvents="none">
-          <Text style={{fontSize: 13, paddingLeft: 132, color: '#3C65CC',  marginBottom: 7, marginTop: -7}} >Information Technology </Text>
-          <Text style={{fontSize: 16, left: 45, top: -5, fontWeight: 'bold'}} >Web/app designer required to create web apps </Text>
-          <Text style={{fontSize: 13, left: 45, color: '#50555C', marginTop: 2}} >Blackwood, WA </Text>
-          <Text style={{fontSize: 11, paddingLeft: 149, color: '#929599', marginBottom: -8, marginTop: 13}} >Posted 51 minnutes ago </Text>
+        <Card containerStyle={styles.cardLayout} name="card"  pointerEvents="none">
+          <Text style={styles.positionCategory} >Information Technology </Text>
+          <Text style={styles.positionTitle} >Web/app designer required to create web apps </Text>
+          <Text style={styles.positionLocation} >Blackwood, WA </Text>
+          <Text style={styles.postTime} >Posted 51 minnutes ago </Text>
         </Card>
-        <Card style={styles.cardLayout} name="card"  pointerEvents="none">
-          <Text style={{fontSize: 13, paddingLeft: 193, color: '#3C65CC',  marginBottom: 7, marginTop: -7}} >UI/UX Design </Text>
-          <Text style={{fontSize: 16, left: 45, top: -5, fontWeight: 'bold'}} >SaaS Homepage redesign and onboarding updates </Text>
-          <Text style={{fontSize: 13, left: 45, color: '#50555C', marginTop: 2}} >Albany, NY </Text>
-          <Text style={{fontSize: 11, paddingLeft: 149, color: '#929599', marginBottom: -8, marginTop: 13}} >Posted 51 minnutes ago </Text>
+        <Card containerStyle={styles.cardLayout} name="card"  pointerEvents="none">
+          <Text style={styles.positionCategory} >Technology </Text>
+          <Text style={styles.positionTitle} >Adobe Systems Careers - Performance Media, Analyst </Text>
+          <Text style={styles.positionLocation} >Santa Ana, CA </Text>
+          <Text style={styles.postTime} >Posted 24 days ago </Text>
         </Card>
       </View>
-
-      </ScreenContainer>
+      
+      <View style={{padding: 85}}></View>
+      </View>
       </ScrollView>
     );
   }
@@ -111,19 +104,16 @@ export const JobScreen = () => {   //Jobs page component
     );
   }
 
-  const styles = StyleSheet.create({     
-    scrollView: {
-      backgroundColor: '#F7F5F9',
-    },
+  const styles = StyleSheet.create({
 
-    container: {
+    jobContainer: {
       flex: 1,
-      backgroundColor: '#c3bffa',
+      backgroundColor: '#F7F5F9',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'center'
     },
 
-    button: {
+    jobButton: {
       borderRadius: 10,
       width: 153,
       height: 35,
@@ -141,26 +131,76 @@ export const JobScreen = () => {   //Jobs page component
     },
 
     inputContainer: {
-      width: 300,
-      height: 36.59,
-      left: 36,
-      top: 25,
+      width: wp('81%'),
+      height: hp('6%'),
+      left: hp('0.55%'),
+      top: hp('3.5%'),
     },
     
     input: {
-      height: 40,
-      padding: 10,
+      height: hp('5%'),
+      padding: hp('1.5%'),
       borderWidth: 1,
-      borderRadius: 5,
+      borderRadius: 16,
       backgroundColor: 'white',
-      borderColor: '#DADADA',
+      borderColor: '#4C67F6',
       fontStyle: 'italic'
     },
 
-    cardLayout: {
-      //justifyContent: 'flex-start',
-      flexDirection: 'row',
-      width: 200,
-      height: 100,
+    jobTitle: {
+      color:'#FF6E00',
+      fontSize: 24,
+      fontWeight:'bold',
+      paddingTop: hp('3.5%'),
+      paddingBottom: hp('1%'),
     },
+
+    jobSubtitle: {
+      color: "#3F3356",
+      fontSize: 24,
+      fontWeight:'bold',
+      paddingBottom: hp('1%'),
+    },
+
+    cardLayout: {
+      backgroundColor: 'white',
+      borderWidth: 0, // Remove Border
+      shadowColor: 'rgba(0,0,0, 0.0)', // Remove Shadow IOS
+      shadowOffset: { height: 0, width: 0 },
+      shadowOpacity: 0,
+      shadowRadius: 0,
+      elevation: 0, // This is for Android
+      borderRadius: 15,
+    },
+
+    positionCategory: {
+      fontSize: 13,  
+      color: '#3C65CC',  
+      marginBottom: 13, 
+      marginTop: -7,
+      textAlign: 'right',
+    },
+
+    positionTitle: {
+      fontSize: 16, 
+      left: hp('6%'), 
+      top: -5, 
+      fontWeight: 'bold',
+    },
+
+    positionLocation: {
+      fontSize: 13, 
+      left: hp('6.1%'), 
+      color: '#50555C', 
+      marginTop: 2
+    },
+
+    postTime: {
+      fontSize: 11, 
+      textAlign: 'right',
+      marginLeft: 149, 
+      color: '#929599', 
+      marginBottom: -8, 
+      marginTop: 13,
+    }
 });
