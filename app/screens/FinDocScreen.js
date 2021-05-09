6d7f2978982c99a1cfc317e59542ed84cd6a9acd
Ashley Zhou <adzhou@uci.edu>
2021-05-09 13:03:47 -0700

    Update FinDocScreen.js

--- app/screens/FinDocScreen.js
+++ app/screens/FinDocScreen.js
@@ -5,7 +5,6 @@ import { TouchableHighlight } from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 // for responsive design 
 import { heightPercentageToDP as hp , widthPercentageToDP as wp } from 'react-native-responsive-screen';
-// import { Picker } from '@react-native-picker/picker'
 import DropDownPicker from 'react-native-dropdown-picker';
 
 
@@ -13,32 +12,98 @@ import DropDownPicker from 'react-native-dropdown-picker';
 // to submit to the database so their application process is manually reviewed
 // Part of financial aid stack defined in App.js, directly from landing page
 export const FinAppScreen = ({navigation}) => {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
+    // Set variables and states for opening dropdown menus 
+    const [visa_open, setVisaOpen] = useState(false);
+    const [visa_value, setVisaValue] = useState(null);
+
+    const [marital_open, setMaritalOpen] = useState(false);
+    const [marital_value, setMaritalValue] = useState(null);
+
+    const [permit_open, setPermitOpen] = useState(false);
+    const [permit_value, setPermitValue] = useState(null);
+
+    // Set all dropdown menu choices and their values
+    const [visa_choices, setVisaItems] = useState([
+        {label: 'F-1', value: 'f1'},
+        {label: 'J-1', value: 'j1'}, 
+        {label: 'DACA', value: 'daca'},
+        {label: 'Green Card (permanent resident)', value: 'green_card'},
+        {label: 'Other', value: 'other'}
+    ]);
+
+    const [marital_choices, setMaritalItems] = useState([
         {label: 'Single', value: 'single'},
         {label: 'Married', value: 'married'}, 
         {label: 'Widowed', value: 'widowed'},
         {label: 'Divorced', value: 'divorced'},
         {label: 'Domestic Partnership', value: 'partnership'}
     ]);
-    
+
+    const [permit_choices, setPermitItems] = useState([
+        {label: 'F-1 without work authorization', value: 'f1_no_work'},
+        {label: 'F-1 Hardship EAD (work permit)', value: 'work_permit'}, 
+        {label: 'F-1 CPT', value: 'cpt'},
+        {label: 'F-1 Pre-Completion OPT', value: 'pre-opt'},
+        {label: 'F-1 Regular OPT', value: 'regular_opt'},
+        {label: 'F-1 STEM OPT', value: 'stem_opt'},
+        {label: 'J-1 work authorized', value: 'j1_work'}
+    ]);
+
     return (
         <ScrollView>
             <View style={styles.container}>
             {/* Back button that allows the user to go back to the landing screen */}
             <TouchableHighlight onPress={() => navigation.goBack()} underlayColor={'#F7F5F9'}>
-            {/* backgroundColor: '#F7F5F9' */}
                 <View style={styles.backButtonContainer}>
                     <Ionicons name="arrow-back-circle" size={40} color="#4C67F6"/>
                 </View>
             </TouchableHighlight>
 
             <Text style={styles.header}>Select the type of visa you're current holding</Text>
-            {/* Dropdown here */}
+            <DropDownPicker
+                // Set menu choices
+                items = {visa_choices}
+                // Set the open state
+                open = {visa_open}
+                // Set the value of the selected choice
+                value = {visa_value}
+                setValue = {setVisaValue}
+                setitems = {setVisaItems}
+                setOpen = {setVisaOpen}
+                // There's no search bar
+                searchable = {false}
+                // Making it scrollable
+                listMode = "SCROLLVIEW"
+                // The height of the dropdown 
+                maxHeight = {150} 
+                // Added alignSelf to make container center
+                style = {{width: wp('76%'), alignSelf:"center"}} 
+                // Added alignSelf center to make dropdown center
+                containerStyle = {{width: wp('76%'), alignSelf:"center"}} 
+            />
+
+            <Text style={styles.header}>If you put "Other" in the last question, please put in the visa you carry. 
+            Otherwise, put "N/A" (without quotes).</Text>
+                <View style={styles.inputContainer}>
+                    <View style={{paddingVertical: hp('1%')}}>
+                        <TextInput
+                            style={styles.shortAnswerInput}
+                            // onChangeText={onChangeText}
+                            // onChangeText={(text) => {this.setState({text});}}
+                            // value={this.state.text}
+                            // multiline onChange={(event) => this.onChangeText(event)}
+                            // onChange={(event) =>
+                            //     this.onChangeText(event)
+                            //   }
+                            //   onContentSizeChange={(event) => {
+                            //     this.setState({ height: event.nativeEvent.contentSize.height })
+                            //   }}
+                        />
+                </View>
+            </View>
 
             <Text style={styles.header}>Please elaborate on why you identify as an international student. Include details such as 
-            country of origin, the process you undertook to get your visas and obtain your status, etc.</Text>
+            country of origin, the process you undertook to get your visa(s) and obtain your status, etc.</Text>
             <View style={styles.inputContainer}>
                 <View style={{paddingVertical: hp('1%')}}>
                     <TextInput
@@ -109,55 +174,34 @@ export const FinAppScreen = ({navigation}) => {
                 </View>
             </View>
 
-            {/* Make this a drop down */}
             <Text style={styles.header}>Marital Status</Text>
             <DropDownPicker
-                items = {items}
-                    
-                //What I changed, Stephanie
-                open={open}
-                value={value}
-                setValue={setValue}
-                setitems={setItems}
-                setOpen={setOpen}
-                searchable={false}
-                listMode="SCROLLVIEW" //Making it scrollable
-                maxHeight={150} //The height of the dropdown
-                //
-
-                // {[{label: 'Single', value: 'single'},
-                // {label: 'Married', value: 'married'}, 
-                // {label: 'Widowed', value: 'widowed'},
-                // {label: 'Divorced', value: 'divorced'},
-                // {label: 'Domestic Partnership', value: 'partnership']}
-                // open = {open}
-                // value = {value}
-                // items = {items}
-                // setValue = {setValue}
-                // setItems = {setItems}
-                // setopen = {setOpen}
-
-                //What I changed, Stephanie
-                style = {{width: wp('76%'), alignSelf:"center"}} //Added alignSelf to make container center
-                containerStyle = {{width: wp('76%'), alignSelf:"center"}} //Added alignSelf center to make dropdown center
-            /> 
-
-{/* const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        {label: 'Single', value: 'single'},
-        {label: 'Married', value: 'married'}, 
-        {label: 'Widowed', value: 'widowed'},
-        {label: 'Divorced', value: 'divorced'},
-        {label: 'Domestic Partnership', value: 'partnership'}
-    ]); */}
-            
+                // Set menu choices
+                items = {marital_choices}
+                // Set the open state
+                open = {marital_open}
+                // Set the value of the selected choice
+                value = {marital_value}
+                setValue = {setMaritalValue}
+                setitems = {setMaritalItems}
+                setOpen = {setMaritalOpen}
+                // There's no search bar
+                searchable = {false}
+                // Making it scrollable
+                listMode = "SCROLLVIEW"
+                // The height of the dropdown 
+                maxHeight = {150} 
+                // Added alignSelf to make container center
+                style = {{width: wp('76%'), alignSelf:"center"}} 
+                // Added alignSelf center to make dropdown center
+                containerStyle = {{width: wp('76%'), alignSelf:"center"}} 
+            />            
 
             <Text style={styles.header}>If you have children, please note how many</Text>
             <View style={styles.inputContainer}>
                 <View style={{paddingVertical: hp('1%')}}>
                     <TextInput
-                        style={styles.maritalInput}
+                        style={styles.childrenInput}
                         // onChangeText={onChangeText}
                         // onChangeText={(text) => {this.setState({text});}}
                         // value={this.state.text}
@@ -172,29 +216,46 @@ export const FinAppScreen = ({navigation}) => {
                 </View>
             </View>
 
-            {/* Change this */}
-            <Text style={styles.header}>Have you gotten or are you planning to get an OPT, work permit, STEM OPT, etc.?</Text>
-            {/* <Picker
-                // selectedValue={selectedLanguage}
-                // onValueChange={(itemValue) =>
-                //     setSelectedLanguage(itemValue)}
-            >
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
-            </Picker> */}
+            <Text style={styles.header}>Select the work permit you currently carry</Text>
+            <DropDownPicker
+                // Set dropdown menu choices
+                items = {permit_choices}
+                // Set the open state
+                open = {permit_open}
+                // Set the value of the selected choice
+                value = {permit_value}
+                setValue = {setPermitValue}
+                setitems = {setPermitItems}
+                setOpen = {setPermitOpen}
+                // There is no search bar
+                searchable = {false}
+                // Making it scrollable
+                listMode = "SCROLLVIEW"
+                // The height of the dropdown 
+                maxHeight = {150} 
+                // Added alignSelf to make container center
+                style = {{width: wp('76%'), alignSelf:"center"}} 
+                // Added alignSelf center to make dropdown center
+                containerStyle = {{width: wp('76%'), alignSelf:"center"}} 
+            />
 
-            <Text style={styles.header}>If yes, please explain where you are in the process and 
-            the current situation around the application process. If no, please put "N/A".</Text>
-            <View style={styles.inputContainer}>
-                <View style={{paddingVertical: hp('1%')}}>
-                    <TextInput
-                        style={styles.input}
-                        // onChangeText={(text) => {this.setState({text});}}
-                        // value={this.state.text}
-                        multiline={true}
-                        numberOfLines={4}
-                        textAlignVertical={'top'}
-                    />
+            <Text style={styles.header}>If you put "Other" in the last question, please put in the visa you carry. 
+            Otherwise, put "N/A" (without quotes).</Text>
+                <View style={styles.inputContainer}>
+                    <View style={{paddingVertical: hp('1%')}}>
+                        <TextInput
+                            style={styles.shortAnswerInput}
+                            // onChangeText={onChangeText}
+                            // onChangeText={(text) => {this.setState({text});}}
+                            // value={this.state.text}
+                            // multiline onChange={(event) => this.onChangeText(event)}
+                            // onChange={(event) =>
+                            //     this.onChangeText(event)
+                            //   }
+                            //   onContentSizeChange={(event) => {
+                            //     this.setState({ height: event.nativeEvent.contentSize.height })
+                            //   }}
+                        />
                 </View>
             </View>
 
@@ -270,7 +331,6 @@ const styles = StyleSheet.create({
       backgroundColor: '#F7F5F9',
     //   alignItems: 'center',
       justifyContent: 'center',
-    //   height: keyboard.keyboardShown? 510 - keyboard.keyboardHeight: 510
     },
 
     // Defines styling for the back button
@@ -333,7 +393,6 @@ const styles = StyleSheet.create({
         marginVertical: wp('1%'),
     },
 
-    // make new lines when text reaches end of box
     // Defines the long form text boxes themselves
     input: {
         height: hp('30%'),
@@ -345,7 +404,6 @@ const styles = StyleSheet.create({
         borderColor: '#DADADA'
       },
 
-    // make new lines when text reaches end of box
     // Defines how user inputs text in all short answer textboxes (amount of money, phone num, etc.)
     shortAnswerInput: {
         height: hp('5.2%'),
@@ -357,7 +415,8 @@ const styles = StyleSheet.create({
         borderColor: '#DADADA',
       },
 
-    maritalInput: {
+    // Tiny input text for number of children 
+    childrenInput: {
         height: hp('5.2%'),
         width: wp('10%'),
         padding: wp('3%'),
