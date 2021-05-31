9ca310ebced9196927f6ec52f1669ee1fa746328
architab-git <32461123+Archi6828@users.noreply.github.com>
2021-05-30 17:05:18 -0700

    updated home page

--- app/screens/home.js
+++ app/screens/home.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text,  StyleSheet, ScrollView,  TouchableOpacity,Image } from "react-native";
 import { AuthContext } from '../screens/context';
 import { ScreenContainer } from 'react-native-screens';
+import LegalCard from '../screens/legalCard';
 import {
     heightPercentageToDP as hp,
     widthPercentageToDP as wp,
@@ -11,43 +12,62 @@ import {
 This is the home page. It contains the cards for Upcomming Appointments 
 Saved Financial Forms. It also links users to the CFGI News and User Profile page.
 */
+const users = [
+    {
+       name: 'Bill Gunadi',
+       expertise: 'OPT, CPT, H1B',
+       languages: 'English, Hindi',
+       image: '../assets/img/attorneydefault.png',
+       key:'1'
+    },
+    {
+      name: 'Taylor Mason',
+      expertise: 'OPT, CPT, H1B',
+      languages: 'English, Hindi',
+      image: '../assets/img/attorneydefault.png',
+      key:'2'
+   },
+    {
+      name: 'Kristine Park',
+      expertise: 'OPT, CPT, H1B',
+      languages: 'English, Hindi',
+      image: '../assets/img/attorneydefault.png',
+      key:'3'
+  }
+   ]
+
  export const HomeScreen = ({navigation}) => { 
+    //List of Cards of saved Financial Applications
+    var cards =  users.map(u => {
+    return (
+        <View key={u.key} name = {u.name} expertise = {u.expertise} languages = {u.languages} onPress={() => navigation.navigate('calendar')}>
+        </View>
+    );}
+    );
     return (
         <ScrollView>
             <ScreenContainer style={styles.container}>
-            {/* top row of navigation */}
+            {/* logo */}
             <View style={styles.inRow}> 
-                <TouchableOpacity  onPress={() => navigation.navigate("About")}>
-                    <Image style={ styles.icon } source={require("../assets/img/CFGInews.png")}/>
-                </TouchableOpacity>
                 <Image style={styles.logo} source={require("../assets/img/logo-full.png")}/>
-                <TouchableOpacity  onPress={() => navigation.navigate("Profile")}>
-                <Image style={ styles.icon }  source={require("../assets/img/profile.png")}/>
-                </TouchableOpacity>
             </View>
 
-            {/* cards for upcomming appointments */}
-            <View style={styles.section1}>
-                <Text style={styles.h1}>Upcomming Appointments</Text>
-            
-                <TouchableOpacity  onPress={() => navigation.navigate("Legal")}>
-                <View  style={styles.cardLayout} name="card"  pointerEvents="none">
-                    <Image style={styles.PersonImg} source={require("../assets/img/attorney.png")} />         
-                    <Text style={styles.section1_Txt} >Attorney: Toby Renolds{"\n"}{"\n"}Starting @ 7:00 AM - May 8, 2019 </Text>
-                    <Image  style={{resizeMode:"contain"}} source={require("../assets/img/rightArrow.png")} />         
-                </View>
-                </TouchableOpacity>
-            </View> 
-
             {/* cards for saved financial forms */}
+           
             <View style={styles.section2}>
                 <Text style={styles.h1}>Saved Financial Forms</Text>
+                <Text style={styles.subh1}>Financial forms not yet submitted</Text>
+                {/* Where the cards populate 
+                <View style={{padding: 30}}>
+                {cards}
+                </View>
+                */ }
                 
                 <TouchableOpacity  onPress={() => navigation.navigate("Financial")}>
                 <View style={styles.cardLayout} name="card"  pointerEvents="none">
                     <Image style={styles.FormImg} source={require("../assets/img/notSubmittedFin.png")} />         
-                    <Text style={styles.section2_Txt}>Status: not yet submitted{"\n"}{"\n"}Submitted at 7:00 AM - May 8, 2019 </Text>
-                    <Text style={{ fontSize: 12, textAlign: 'right',color: 'red', marginTop: -2, textDecorationLine: 'underline'}}>Delete draft</Text>
+                    <Text style={styles.section2_Txt}>Saved at 7:00 AM - May 8, 2019 </Text>
+                    <Text style={styles.deleteText}>Delete draft</Text>
                 </View>
                 </TouchableOpacity>
             </View> 
@@ -59,7 +79,7 @@ Saved Financial Forms. It also links users to the CFGI News and User Profile pag
 
   const styles = StyleSheet.create({   
     inRow: {
-        flexDirection: 'row',
+        flexDirection: 'row', backgroundColor: '#F7F5F9',
       },
 
     icon:{
@@ -70,25 +90,32 @@ Saved Financial Forms. It also links users to the CFGI News and User Profile pag
         width: 80, height: 80, resizeMode:"contain", 
     },
     FormImg: {
-        width: 70, height: 70, resizeMode:"contain", 
+         width: 50, height: 50, resizeMode:"contain", 
     },
 
     section1_Txt: {
         fontSize: 16, left: 10, top: 10
     },
     section2_Txt: {
-        fontSize: 13, left: 10, top: 10
+        fontSize: 15, left: 10, top: 22
     },
 
     h1: {
-         fontSize: 22, fontWeight: 'bold', left: 15,
+         fontSize: 22, fontWeight: 'bold',
     },
-    
-    section1: {
-        top: 50, paddingEnd: 20,
+    subh1: {
+        fontSize: 18, left: 5, padding: 5
+    },
+    deleteText: {
+        fontSize: 12,
+        textAlign: 'right',
+        color: 'red',
+        marginTop: 3,
+        textDecorationLine: 'underline',
+        left: -10
     },
     section2: {
-        top: 100, paddingEnd: 20,
+        top: 100, paddingEnd: 20,   backgroundColor: '#F7F5F9',
     },
 
     logo:{
@@ -109,6 +136,6 @@ Saved Financial Forms. It also links users to the CFGI News and User Profile pag
         elevation: 1,
         marginTop: 10,
         borderRadius: 10,
-        padding: 10,
+        padding: 2,
     },
 });
